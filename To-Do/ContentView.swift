db34fb2c2374e81b46047f46ad18bdab029fff5b
Adnan Joraid <adnanjoraidca@gmail.com>
2020-07-22 19:52:30 -0400

    Initial Commit

---
+++ To-Do/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do
+//
+//  Created by Adnan Joraid on 2020-07-22.
+//  Copyright © 2020 Adnan Joraid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
